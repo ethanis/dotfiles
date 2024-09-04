cb0064958f921c65ff712b74a48a3647ef091fe9
Ethan Dennis <ethanis@Ethans-MacBook-Pro-2.local>
2024-09-03 17:37:42 -0700

    Initial install script

---
+++ install.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env ruby
+require 'erb'
+
+puts "# Syncing to home folder...\n"
+
+def link_file file
+  if file =~ /.erb$/
+    puts "generating ~/.#{file.sub('.erb', '')}"
+    File.open(File.join(ENV['HOME'], ".#{file.sub('.erb', '')}"), 'w') do |new_file|
+      new_file.write ERB.new(File.read(file)).result(binding)
+    end
+  else
+    puts "linking ~/.#{file}"
+    system %Q{ln -s "$PWD/#{file}" "$HOME/.#{file}"}
+  end
+end
+
+def install
+  replace_all = false
+
+  Dir['*'].each do |file|
+    next if %w[Readme.md LICENSE install.sh].include? file
+    
+    if File.exist?(File.join(ENV['HOME'], ".#{file.sub('.erb', '')}"))
+      if File.identical? file, File.join(ENV['HOME'], ".#{file.sub('.erb', '')}")
+        puts "identical ~/.#{file.sub('.erb', '')}"
+      elsif replace_all
+        replace_file(file)
+      else
+        print "overwrite ~/.#{file.sub('.erb', '')}? [ynaq] "
+        case $stdin.gets.chomp
+        when 'a'
+          replace_all = true
+          replace_file(file)
+        when 'y'
+          replace_file(file)
+        when 'q'
+          exit
+        else
+          puts "skipping ~/.#{file.sub('.erb', '')}"
+        end
+      end
+    else
+      link_file(file)
+    end
+  end
+end
+
+
+def link_files
+    Dir['*'].each do |file|
+      next if %w[Rakefile Readme.md LICENSE install.sh].include? file
+
+      link_file file
+    end
+    0
+end
+
+link_files
+
+puts ""
+puts "All done!"
